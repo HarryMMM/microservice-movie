87ecee825d26caf98e317b0f745b92d983305646
harry <hhr_harry@qq.com>
2018-12-27 11:51:12 +0800

    增加打印用户服务实例

--- src/main/java/cn/harryai/microservicemovie/controller/MovieController.java
+++ src/main/java/cn/harryai/microservicemovie/controller/MovieController.java
@@ -4,22 +4,43 @@ import cn.harryai.microservicemovie.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
     private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        User findOne = this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+        User findOne = this.restTemplate.getForObject("http://microservice-user/" + id, User.class);
         LOGGER.info("user is {}", findOne);
         return findOne;
+    }
+
+    @GetMapping("/log-user-instance")
+    public ServiceInstance logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-user");
+        LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+        return serviceInstance;
+    }
+
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo() {
+        return discoveryClient.getInstances("microservice-user");
     }
 
 }
